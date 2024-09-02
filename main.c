f7403c02a56439ee53152d035ccc67f34c48e0b9
rickd-uk <dev.bites.uk@gmail.com>
2024-09-02 10:36:10 +0900

    allows commands to be typed; ctr+q to quit

--- main.c
+++ main.c
@@ -1,16 +1,42 @@
 #include <ncurses.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+
+// macro to convert a char to its ctrl-key equivalent
+#define ctrl(x) ((x) & 0x1f)  
+
 
 int main() {
-  initscr();
-  raw();
-  noecho();
+  initscr(); // initialize ncurses screen
+  raw();     // disable line buffering
+  noecho();  // disable automatic encoding of characters
 
-  printw("Test");
-  int ch = getch();
+  int ch;
+  bool QUIT = false;
+
+  char command[1024] = {0};  // buffer to store a command
+  size_t command_s = 0;   // current size of command
+
+  while (!QUIT) {
+    printw("shell $: ");   // display shell prompt
+    printw(command);       // print current command
+    ch = getch();
+    switch (ch) {
+    case ctrl('q'):      // ctrl+q  to quit
+      QUIT = true;
+      break;
+    default:
+      // add char to command buffer
+      command[command_s++] = ch;
+      break;
+    }
+    erase();
+  }
 
   refresh();
-  endwin();
+  endwin(); // restore normal terminal behavior
 
   return 0;
 }
